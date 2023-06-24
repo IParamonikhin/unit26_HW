c8716625ee57ae69ec3623a11f49907c19244be3
Ivan <paramonikhin.ivan@yandex.ru>
2023-06-24 23:06:59 +0300

    first

--- unit26_HW/ViewController.swift
+++ unit26_HW/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switchBtn: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let defaultValue = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        switchBtn.setTitle("", for: .normal)
+        switchBtn.layer.cornerRadius = switchBtn.frame.height / 2
+        switchBtn.backgroundColor = .yellow
+        if defaultValue.string(forKey: "theme") == "day"{
+            switchBtn.setImage(UIImage(systemName: "sun.max"), for: .normal)
+            imageView.image = UIImage(named: "day")
+            self.view.backgroundColor = UIColor(named: "MyLightBlue")
+        } else {
+            switchBtn.setImage(UIImage(systemName: "moon"), for: .normal)
+            imageView.image = UIImage(named: "night")
+            self.view.backgroundColor = UIColor(named: "MyDarkBlue")
+        }
+    
+        
     }
 
 
+    @IBAction func switchBtnAction(_ sender: Any) {
+        if defaultValue.string(forKey: "theme") == "day"{
+            defaultValue.set("night", forKey: "theme")
+            switchBtn.setImage(UIImage(systemName: "moon"), for: .normal)
+            imageView.image = UIImage(named: "night")
+            self.view.backgroundColor = UIColor(named: "MyDarkBlue")
+        } else {
+            defaultValue.set("day", forKey: "theme")
+            switchBtn.setImage(UIImage(systemName: "sun.max"), for: .normal)
+            imageView.image = UIImage(named: "day")
+            self.view.backgroundColor = UIColor(named: "MyLightBlue")
+        }
+        reloadInputViews()
+    }
 }
 
