d5e8c4bda59c0c901b97f51147562b16da4c9094
Ivan <paramonikhin.ivan@yandex.ru>
2023-06-24 21:51:58 +0300

    Initial Commit

---
+++ unit26_HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  unit26_HW
+//
+//  Created by Иван on 24.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
